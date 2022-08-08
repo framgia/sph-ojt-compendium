867dc3f7e424b980fe5e41329bf504a0e10d0542
EN Rex George <enrexgeorge.verdida@sun-asterisk.com>
2022-08-08 19:12:52 +0800

    adding migration task

---
+++ be/compendium/db/models.py
@@ -0,0 +1,33 @@
+from contextlib import nullcontext
+from tkinter import CASCADE
+from django.db import models
+
+# Create your models here.
+
+
+class Accounts(models.Model):
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+
+
+class Users(models.Model):
+    account_id = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    birth_date = models.DateTimeField(auto_now=True)
+    sex = models.CharField(max_length=6)
+    school = models.CharField(max_length=50)
+
+
+class Daily_reports(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    progress = models.TextField()
+    problems = models.TextField(default=0)
+    learnings = models.TextField()
+    plans = models.TextField()
+    date_created = models.DateTimeField(auto_now=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    date_deleted = models.DateTimeField(auto_now=True)
+    rate_for_value_delivered = models.IntegerField(default=0)
