988bceba57adc6faa499a72db46c8300ea2b6432
biancanailon <ninabianca.nailon@sun-asterisk.com>
2022-08-10 16:23:23 +0800

    Implemented a route for create

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import CREATE_DAILY_REPORT from './pages/CREATE_DAILY_REPORT';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <CREATE_DAILY_REPORT/>
+      <Switch>
+        <Route path = "/report/create"></Route>
+        <CREATE_DAILY_REPORT/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
