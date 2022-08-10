d2a54546fb460a5750d9306d77c09735206aff1a
biancanailon <ninabianca.nailon@sun-asterisk.com>
2022-08-10 16:57:43 +0800

    formatted the code

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import CREATE_DAILY_REPORT from './pages/CREATE_DAILY_REPORT';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import "./App.css";
+import CREATE_DAILY_REPORT from "./pages/CREATE_DAILY_REPORT";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Switch>
-        <Route path = "/report/create"></Route>
-        <CREATE_DAILY_REPORT/>
-      </Switch>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route path="/report/create"></Route>
+          <CREATE_DAILY_REPORT />
+        </Switch>
+      </div>
     </Router>
   );
 }
