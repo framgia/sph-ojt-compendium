8d0226b2bf2a3cb054654390eac2647c234f96b1
Roy <roy.donoz@sun-asterisk.com>
2022-08-12 11:45:52 +0800

    [SPH-TASK] [BE] Daily Report (Create)

--- be/compendium/db/models.py
+++ be/compendium/db/models.py
@@ -1,33 +1,33 @@
 from contextlib import nullcontext
+from doctest import BLANKLINE_MARKER
 from tkinter import CASCADE
 from django.db import models
 
+
 # Create your models here.
-
-
 class Accounts(models.Model):
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
 
-
 class Users(models.Model):
-    account_id = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    account = models.ForeignKey(
+        Accounts, on_delete=models.CASCADE, null=True)
     first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, null=True, blank=True)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    birth_date = models.DateTimeField(auto_now=True)
-    sex = models.CharField(max_length=6)
-    school = models.CharField(max_length=50)
+    birth_date = models.DateField(null=True, blank=True, default=0)
+    sex = models.CharField(max_length=6, null=True, blank=True)
+    school = models.CharField(max_length=50, null=True, blank=True)
 
 
 class Daily_reports(models.Model):
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     progress = models.TextField()
-    problems = models.TextField(default=0)
+    problems = models.TextField(blank=True, null=True)
     learnings = models.TextField()
     plans = models.TextField()
     date_created = models.DateTimeField(auto_now=True)
-    last_updated = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now=True)
-    rate_for_value_delivered = models.IntegerField(default=0)
+    last_updated = models.DateTimeField(auto_now=True, null=True)
+    date_deleted = models.DateTimeField(null=True,blank=True )
+    rate_for_value_delivered = models.IntegerField(default=0, null=True, blank=True)
