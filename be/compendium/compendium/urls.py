49391c7a6af3585380863c7cf4ae24c6d6f5131b
vandam <armandvan.romanillos@sun-asterisk.com>
2022-08-18 10:46:25 +0800

    draft-update-daily-report

--- be/compendium/compendium/urls.py
+++ be/compendium/compendium/urls.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
+from django.contrib import admin
+from django.urls import path
 from db.views import DailyReportAPIView
 from db import views
-
 from db.views import DailyReportListAPIView, DailyReportRetrieveUpdateDestroyAPIView
 
+
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.authtoken')),
     path('dailyReports/', DailyReportListAPIView.as_view()),
-    path('dailyReports/<int:pk>/', DailyReportRetrieveUpdateDestroyAPIView.as_view()),
+    path('dailyReports/<int:pk>/',
+         DailyReportRetrieveUpdateDestroyAPIView.as_view()),
     path('daily-report/create/', DailyReportAPIView.as_view()),
+    path('dailyReports/<int:pk>/update',
+         DailyReportRetrieveUpdateDestroyAPIView.as_view()),
 ]
--- be/compendium/db/models.py
+++ be/compendium/db/models.py
@@ -1,6 +1,6 @@
-from contextlib import nullcontext
 from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
@@ -8,25 +8,33 @@ class Accounts(models.Model):
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
 
-class Users(models.Model):
-    account = models.ForeignKey(
-        Accounts, on_delete=models.CASCADE, null=True)
+
+class User(AbstractUser):
+    # account = models.ForeignKey(
+    #     Accounts, on_delete=models.CASCADE, null=True)
     first_name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50, null=True, blank=True)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    birth_date = models.DateField(null=True, blank=True, default=0)
+    email = models.CharField(max_length=50, unique=True)
+    birth_date = models.DateField(null=True, blank=True)
     sex = models.CharField(max_length=6, null=True, blank=True)
     school = models.CharField(max_length=50, null=True, blank=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', ]
+
+    def get_username(self):
+        return self.email
+
 
 class Daily_reports(models.Model):
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     progress = models.TextField()
     problems = models.TextField(blank=True, null=True)
     learnings = models.TextField()
     plans = models.TextField()
     date_created = models.DateTimeField(auto_now=True)
     last_updated = models.DateTimeField(auto_now=True, null=True)
-    date_deleted = models.DateTimeField(null=True,blank=True )
-    rate_for_value_delivered = models.IntegerField(default=0, null=True, blank=True)
+    date_deleted = models.DateTimeField(null=True, blank=True)
+    rate_for_value_delivered = models.IntegerField(
+        default=0, null=True, blank=True)
